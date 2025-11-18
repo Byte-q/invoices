b9f4fc24522cfaa95b1e839c7ddb0bb01e5e14b2
Mohammed abdalaziz <mohaazizz01@gmail.com>
2025-11-18 17:26:42 +0200

    fix false import

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,7 +9,6 @@ import {
   CardsSkeleton,
 } from "@/app/ui/skeletons";
 import { Metadata } from "next";
-import { ChartAreaDefault } from "@/components/ui/RevenueChart";
 
 export const metadata: Metadata = {
   title: "Dashboard",
